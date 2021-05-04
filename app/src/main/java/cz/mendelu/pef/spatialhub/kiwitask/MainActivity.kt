44bff64bb17d79ab2a207a722e739e26d4238141
ondrejivan <xivan@mendelu.cz>
2021-05-04 09:09:15 +0200

    Init commit

---
+++ app/src/main/java/cz/mendelu/pef/spatialhub/kiwitask/MainActivity.kt
@@ -0,0 +1,11 @@
+package cz.mendelu.pef.spatialhub.kiwitask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
